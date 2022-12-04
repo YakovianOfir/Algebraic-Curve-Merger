31958bec1f25e798eae8447e8060c0982d9868b7
Ofir Yakovian <ofiryakovian@mail.tau.ac.il>
2022-12-04 22:40:16 +0200

    [Universe]: Support printing the underlying universe in question

---
+++ Source/Main.py
@@ -0,0 +1,10 @@
+from Universe import *
+
+
+def main():
+    u = Universe(2, 5)
+    u.show()
+
+
+if __name__ == "__main__":
+    main()
--- Source/Universe.py
+++ Source/Universe.py
@@ -1,3 +1,21 @@
 import galois
 
-print(galois.__version__)
+
+class Universe:
+
+    def __init__(self, p, d):
+        self.domain = galois.GF(p ** d, display="poly")
+        assert self.domain.characteristic == p
+        assert self.domain.degree == d
+
+    def __str__(self):
+        return self.domain.properties
+
+    def show(self):
+        print()
+        print("=" * 60)
+        print("Universe -> \n{}\n".format(self))
+        for i, e in enumerate(self.domain.elements):
+            print("Universe Element (#{:06}) -> {}".format(i, str(e)))
+        print("=" * 60)
+        print()
